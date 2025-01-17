b6b5d8f4fc59648170869f843791358af127b750
cshort4403 <christopher.short1@mohawkcollege.ca>
2025-01-17 15:35:45 -0500

    Add classes
    Initial commit with some empty class and folder structure.

---
+++ Dictator Simulator/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The main class for controlling the different interactions between game mechanics.
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
