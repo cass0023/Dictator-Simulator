0fbbc2791535c512456d33de371e1130e8b5d9d1
cshort4403 <christopher.short1@mohawkcollege.ca>
2025-01-17 16:00:36 -0500

    Setup game manager
    Setup the game manager and made the initial office structure.

--- Dictator Simulator/Assets/Scripts/GameManager.cs
+++ Dictator Simulator/Assets/Scripts/GameManager.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// The main class for controlling the different interactions between game mechanics.
 /// </summary>
 public class GameManager : MonoBehaviour
 {
+
+    public int WeekNum = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,4 +23,29 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    /// <summary>
+    /// Load a new scene using the name. It must match exactly.
+    /// </summary>
+    /// <param name="sceneName"></param>
+    public void LoadScene(string sceneName)
+    {
+        if (string.IsNullOrEmpty(sceneName))
+        {
+            Debug.LogError($"Invalid scene name {0}. Check your spelling.");
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+    /// <summary>
+    /// Handle moving to the next week. Change stats as necissary.
+    /// </summary>
+    public void GoToNextWeek()
+    {
+        WeekNum++;
+    }
+
 }
