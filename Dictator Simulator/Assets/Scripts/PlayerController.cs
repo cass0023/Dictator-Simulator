83cc7e0e3dcddb183003941c4360ae449c4e57fa
cass cochrane <150483589+cass0023@users.noreply.github.com>
2025-01-20 17:52:56 -0500

    Add PlayerController

--- Dictator Simulator/Assets/Scripts/PlayerController.cs
+++ Dictator Simulator/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    //does not implement the new input system, will be updating later -cc
+    public bool isMoving, canMove;
+    private float xAxis, yAxis;
+    public float moveSpeed;
+    void Start(){
+        canMove = true;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        xAxis = Input.GetAxis("Horizontal");
+        yAxis = Input.GetAxis("Vertical");
+        CheckMovement();
+        if(canMove){
+            Move();
+        }
+    }
+    void CheckMovement(){
+        // Checks if player is moving
+        if (xAxis != 0 || yAxis != 0)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+    }
+    public void Move(){
+        Vector3 movement = new Vector3(xAxis, 0, yAxis);
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
     }
 }
