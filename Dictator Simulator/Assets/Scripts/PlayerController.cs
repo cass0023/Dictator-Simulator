336ea4eb84e94fc76a4eebbe196283f2f098841b
cass cochrane <150483589+cass0023@users.noreply.github.com>
2025-01-26 10:33:01 -0500

    Adjust PlayerController and add cinemachine
    Add virtual camera to player and change playercontroller code to include camera movement

--- Dictator Simulator/Assets/Scripts/PlayerController.cs
+++ Dictator Simulator/Assets/Scripts/PlayerController.cs
@@ -7,17 +7,28 @@ public class PlayerController : MonoBehaviour
     public bool isMoving, canMove;
     private float xAxis, yAxis;
     public float moveSpeed;
+
+    //Camera variables
+    public float mouseSensitivity;
+    float VertCameraRotate;
+    float cameraAxisX, cameraAxisY;
+    public Transform cameraTransform;
     void Start(){
         canMove = true;
     }
     void Update()
     {
+        //Gets movement input
         xAxis = Input.GetAxis("Horizontal");
         yAxis = Input.GetAxis("Vertical");
         CheckMovement();
         if(canMove){
             Move();
         }
+        //Gets mouse input
+        cameraAxisX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        cameraAxisY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        CameraRotate();
     }
     void CheckMovement(){
         // Checks if player is moving
@@ -33,5 +44,13 @@ public class PlayerController : MonoBehaviour
     public void Move(){
         Vector3 movement = new Vector3(xAxis, 0, yAxis);
         transform.Translate(movement * moveSpeed * Time.deltaTime);
+    }
+    void CameraRotate(){
+        //Vertical rotation
+        VertCameraRotate -= cameraAxisY;
+        VertCameraRotate = Mathf.Clamp(VertCameraRotate, -90f, 90f);
+        cameraTransform.localEulerAngles = Vector3.right * VertCameraRotate;
+        //Horizontal rotation
+        this.transform.Rotate(Vector3.up * cameraAxisX);
     }
 }
