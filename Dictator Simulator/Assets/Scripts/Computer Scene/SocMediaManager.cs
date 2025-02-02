64c14f270a05cb1bce4d59b42994cf2e2998bab5
cass cochrane <150483589+cass0023@users.noreply.github.com>
2025-02-01 22:11:30 -0500

    Add Social Media Posting

---
+++ Dictator Simulator/Assets/Scripts/Computer Scene/SocMediaManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SocMediaManager : MonoBehaviour
+{
+    //temp player stats for debugging
+    public int debugSanity;
+    public int debugApproval;
+    public int debugTrust;
+    public int debugFear;
+
+    //inspector input list for tweet
+    public List<SocMediaEvents> SocMediaEvents;
+    public GameObject[] buttonOptions;
+    //temp number for testing, checking if first input in events is working
+    private int x = 0;
+
+    void Start(){
+        debugApproval = 100;
+        debugFear = 100;
+        debugSanity = 100;
+        debugTrust = 100;
+    }
+    //checks which button is currently pressed and what to display in tweet
+    private bool hasSelected;
+    private int selectedOption;
+    private string tempSelectedOption;
+    public TextMeshProUGUI tweetText;
+    void Update(){
+        //changes tweet text depending on button pressed
+        if(hasSelected){
+            tweetText.text = SocMediaEvents[x].beforeBlank + " " + tempSelectedOption + " " + SocMediaEvents[x].afterBlank;
+        }
+        else{
+            tweetText.text = SocMediaEvents[x].beforeBlank + " _________ " + SocMediaEvents[x].afterBlank;
+        }
+    }
+    //changes selected button to string to display in text
+    public void OnOption1Click(){
+        tempSelectedOption = SocMediaEvents[x].Options[0].name;
+        selectedOption = 0;
+        hasSelected = true;
+    }
+    public void OnOption2Click(){
+        tempSelectedOption = SocMediaEvents[x].Options[1].name;
+        selectedOption = 1;
+        hasSelected = true;
+    }
+    public void OnOption3Click(){
+        tempSelectedOption = SocMediaEvents[x].Options[2].name;
+        selectedOption = 2;
+        hasSelected = true;
+    }
+    public void OnOption4Click(){
+        tempSelectedOption = SocMediaEvents[x].Options[3].name;
+        selectedOption = 3;
+        hasSelected = true;
+    }
+    public void OnOption5Click(){
+        tempSelectedOption = SocMediaEvents[x].Options[4].name;
+        selectedOption = 4;
+        hasSelected = true;
+    }
+    public void OnOption6Click(){
+        tempSelectedOption = SocMediaEvents[x].Options[5].name;
+        selectedOption = 5;
+        hasSelected = true;
+    }
+    public void OnPostClick(){
+        //update stats based on list
+        debugApproval += SocMediaEvents[x].Options[selectedOption].approval;
+        debugFear += SocMediaEvents[x].Options[selectedOption].fear;
+        debugSanity += SocMediaEvents[x].Options[selectedOption].sanity;
+        debugTrust += SocMediaEvents[x].Options[selectedOption].trust;
+        DisableOptionButtons();
+    }
+    private void DisableOptionButtons(){
+        for(int i = 0; i <= buttonOptions.Length; i++){
+            buttonOptions[i].SetActive(false);
+        }
+    }
+}
