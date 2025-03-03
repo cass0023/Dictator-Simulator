eecf741fd7edd11d5a1a2da400de94b79c46bd92
cass cochrane <150483589+cass0023@users.noreply.github.com>
2025-03-03 18:32:07 -0500

    Add camera for calendar and exec order stations

--- Dictator Simulator/Assets/Scripts/PlayerInteract.cs
+++ Dictator Simulator/Assets/Scripts/PlayerInteract.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class PlayerInteract : MonoBehaviour
 {
     //interact variables
-    private bool tvInteract, computerInteract, doorInteract;
+    private bool tvInteract, computerInteract, doorInteract, execOrderInteract, calendarInteract;
     [SerializeField]private KeyCode interact;
     public GameObject newWeekPopUp;
     public GameObject debugMenu;
     private PlayerController playerController;
+    public GameObject interactText;
     void Start()
     {
         playerController = GetComponent<PlayerController>();
         tvInteract = false;
         computerInteract = false;
+        execOrderInteract = false;
+        calendarInteract = false;
         doorInteract = false;
     }
     void Update()
@@ -26,19 +29,7 @@ public class PlayerInteract : MonoBehaviour
         //shows debug menu
         if (Input.GetKeyDown(KeyCode.BackQuote)){
             debugMenu.SetActive(!debugMenu.activeSelf);
-
         }
-        if(Input.GetKeyDown(interact) && tvInteract)
-        {
-            InteractionManager.Instance.SwitchCamera("StatCamera");
-            playerController.canMove = false;
-        }
-		if (Input.GetKeyDown(KeyCode.Escape) && tvInteract || Input.GetKeyDown(KeyCode.Escape) && computerInteract)
-		{
-			InteractionManager.Instance.SwitchCamera("PlayerCam");
-            playerController.AllowMouseMovement();
-            playerController.canMove = true;
-		}
         if(Input.GetKeyDown(interact) && computerInteract){
             InteractionManager.Instance.SwitchCamera("ComCamera");
             //playerController.canMove = false;
@@ -49,8 +40,27 @@ public class PlayerInteract : MonoBehaviour
             Debug.Log("Door interacted");
             newWeekPopUp.SetActive(true);
         }
+        if(Input.GetKeyDown(interact) && execOrderInteract){
+            InteractionManager.Instance.SwitchCamera("ExecOrderCamera");
+        }
+        if(Input.GetKeyDown(interact) && tvInteract)
+        {
+            InteractionManager.Instance.SwitchCamera("StatCamera");
+            playerController.canMove = false;
+        }
+        if (Input.GetKeyDown(interact) && calendarInteract){
+            InteractionManager.Instance.SwitchCamera("CalendarCamera");
+        }
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+            // && tvInteract || Input.GetKeyDown(KeyCode.Escape) && computerInteract
+			InteractionManager.Instance.SwitchCamera("PlayerCam");
+            playerController.AllowMouseMovement();
+            playerController.canMove = true;
+		}
 	}
         void OnTriggerEnter(Collider collider){
+        interactText.SetActive(true);
         if (collider.gameObject.name == "StatScreenZone"){
             tvInteract = true;
         }
@@ -60,11 +70,20 @@ public class PlayerInteract : MonoBehaviour
         if(collider.gameObject.name == "DoorInteractZone"){
             doorInteract = true;
         }
+        if(collider.gameObject.name == "EOInteractZone"){
+            execOrderInteract = true;
+        }
+        if(collider.gameObject.name == "CalendarInteractZone"){
+            calendarInteract = true;
+        }
     }
     void OnTriggerExit(Collider collider){
+        interactText.SetActive(false);
 		tvInteract = false;
         computerInteract = false;
         doorInteract = false;
+        execOrderInteract = false;
+        calendarInteract = false;
         playerController.canMoveMouse = true;
 	}
 }
